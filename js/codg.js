f3f5521f525376a17c3228a9a27d6c19a6cbf32d
Senio Caires <senio.caires@pdcase.com.br>
2021-11-24 18:12:40 -0300

    Half Life

--- js/codg.js
+++ js/codg.js
@@ -15,7 +15,7 @@ var rodadas = [
         games: [
             { nome: 'Fallout: New Vegas', foto: 'fallout-new-vegas.jpg', pontoAdicionar: 5, playerAdicionar: 'marquinhos' },
             { nome: 'Dishonored', foto: 'dishonored.jpg', pontoAdicionar: 10, playerAdicionar: 'camilo' },
-            { nome: '------------------', foto: 'x.jpg', pontoAdicionar: 15, playerAdicionar: '' },
+            { nome: 'Half Life', foto: 'half-life.jpg', pontoAdicionar: 15, playerAdicionar: 'marquinhos' },
             { nome: '------------------', foto: 'x.jpg', pontoAdicionar: 20, playerAdicionar: '' },
             { nome: '------------------', foto: 'x.jpg', pontoAdicionar: 25, playerAdicionar: '' },
             { nome: '------------------', foto: 'x.jpg', pontoAdicionar: 30, playerAdicionar: '' },
